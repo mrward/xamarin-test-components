e37f1dd82ad8f7d327627f164155b7606ffbd4ea
Matt Ward <matt.ward@xamarin.com>
2015-10-19 14:05:32 +0100

    Add WatchOS and tvOS assemblies to components

--- Unified.AwesomeComponent/build.cmd
+++ Unified.AwesomeComponent/build.cmd
@@ -3,7 +3,7 @@ REM    --package="mac-unified":"Newtonsoft.Json, Version=5.0.8" ^
 REM    --package="ios-unified":"Newtonsoft.Json, Version=5.0.8" ^
 
 F:\projects\md-addins\Xamarin.Ide\external\xamarin-component\src\XamComponents\bin\Debug\xamarin-component.exe ^
-    create-manually unified-awesome-1.0.xam ^
+    create-manually unified-awesome-1.1.xam ^
     --name="Unified My Awesome Component" ^
     --summary="Add a huge amount of awesomeness to your Xamarin apps." ^
     --publisher="Awesome Corp, Inc." ^
@@ -15,5 +15,7 @@ F:\projects\md-addins\Xamarin.Ide\external\xamarin-component\src\XamComponents\b
     --icon="icons/unified-awesome_512x512.png" ^
     --library="ios-unified":"bin/Awesome.iOS.dll" ^
     --library="mac-unified":"bin/Awesome.Mac.dll" ^
+    --library="tvos":"bin/Awesome.tvOS.dll" ^
+    --library="watchos":"bin/Awesome.WatchOS.dll" ^
     --sample="iOS Sample. Demonstrates Awesomeness on iOS.":"samples/Awesome.iOS.sln" ^
     --sample="Mac Sample. Demonstrates Awesomeness on Mac":"samples/Awesome.Mac.sln"
